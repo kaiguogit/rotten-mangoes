1751d6c24c98831ce7e49ca42a342a0d6dedb438
kaiguogit <kaiguo1988@gmail.com>
2016-08-18 18:09:54 -0700

    bug fix, changed form action to deal with admin namespace

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
 
   before_filter :restrict_admin_access
 
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -14,11 +14,12 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
     @user = User.new(user_params)
-
     if @user.save
-      session[:user_id] = @user.id
       redirect_to admin_users_path, notice: "Created user #{@user.firstname}"
     else
       render :new
@@ -26,16 +27,20 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(movie_params)
-      redirect_to admin_user_path(@use)
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
     else
-      render :edit
+      redirect_to edit_user_path(@user)
     end
   end
   
   def destroy
-     @user.destroy
-     redirect_to admin_users_path
+    if @user.destroy
+      flash.notice = "Deleted User #{@user.full_name}"
+    else
+      flash.notice = "Cannot delete user #{@user.full_name}"
+    end
+    redirect_to admin_users_path
   end 
 
   protected
