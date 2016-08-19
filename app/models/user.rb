78014e3067d5fc9f477640258040e30b42ed749d
kaiguogit <kaiguo1988@gmail.com>
2016-08-18 18:20:50 -0700

    add checkmark to allow admin set user as admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,27 +12,8 @@ class User < ApplicationRecord
     validates :password,
       length: { in: 6..20 }, on: :create
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to movies_path
-    else
-      render :new
-    end
-  end
-
   def full_name
      "#{firstname} #{lastname}"
   end
 
-  protected
-
-  def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
-  end
 end
